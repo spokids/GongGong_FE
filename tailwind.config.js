c9e691e3efea810eaf453499df74208de503f270
yena0213 <yeana3131@naver.com>
2024-11-20 03:48:53 +0900

    #5 [Chore] 기본 프레임 세팅

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,18 @@ export default {
     './src/**/*.{js,ts,jsx,tsx}', // Include .tsx files
   ],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        mobile: { min: '390px', max: '410px' }, // 커스텀 화면 크기
+      },
+      spacing: {
+        'padding-default': '20px', // 기본 padding 값
+      },
+      container: {
+        center: true, // 기본적으로 가운데 정렬
+        padding: '20px', // 모든 컨테이너에 기본 패딩 적용
+      },
+    },
   },
   plugins: [],
 };
