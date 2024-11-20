3214782f7976f9154bcb5a3e1ec96c4f116c3c05
yena0213 <yeana3131@naver.com>
2024-11-21 00:48:13 +0900

    #5 [Design] 인풋창 퍼블리싱

--- src/pages/login/LoginPage.tsx
+++ src/pages/login/LoginPage.tsx
@@ -1,12 +1,13 @@
 import logoLogin from '../../assets/logoLogin.svg'
+import Input from '../components/input';
 
 const LoginPage = () => {
     return (
-        <div className='flex mt-[223px] content-center justify-center'>
-            <img src={logoLogin} style={{width: '180px'}}/>
-
-            
-        </div>
+    <div className="flex flex-col items-center mt-[223px] space-y-6">
+        <img src={logoLogin} style={{ width: '180px' }} alt="Login Logo" />
+        <Input placeholder="아이디를 입력해주세요." className="mt-4" />
+        <Input type="password" placeholder="비밀번호를 입력해주세요." />
+    </div>
     );
 };
 
