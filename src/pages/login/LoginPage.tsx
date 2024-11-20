6e2b2bce67631b6018ceff6e6d93e94f79271712
yena0213 <yeana3131@naver.com>
2024-11-20 16:59:01 +0900

    #5 [Chore] 이미지들 형식 변경

---
+++ src/pages/login/LoginPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import logoInLogin from '../../assets/logoInLogin';
+
+const LoginPage = () => {
+    return (
+        <div className='mt-[223px] space-between'>
+            <img src='logoInLogin'/>
+        </div>
+    );
+};
+
+export default LoginPage;
