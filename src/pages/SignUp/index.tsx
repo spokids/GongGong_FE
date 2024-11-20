8f268ee49e17608128b9e2aabd37296a65fcabd3
Sohyunnnn <thgus05061@naver.com>
2024-11-21 04:06:29 +0900

    #7 [Feat] 스탭별로 텍스트 다르게 보이도록 구현

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -14,11 +14,18 @@ const SignupPage = () => {
         <h3>회원가입</h3>
       </div>
       <div className="mt-3">
-        <h2>스포키즈에 오신 것을 환영해요!</h2>
-        <h2 className="mt-1">
-          사용하실 아이디와 <br />
-          비밀번호를 설정해주세요.
+        <h2>
+          {step === 1 ? "스포키즈에 오신 것을 환영해요!" : "마지막 단계에요!"}
         </h2>
+        <h2
+          className="mt-1"
+          dangerouslySetInnerHTML={{
+            __html:
+              step === 1
+                ? "사용하실 아이디와 <br />비밀번호를 설정해주세요."
+                : "사용하실 닉네임을 알려주세요.",
+          }}
+        />
       </div>
       {step === 1 && <Step1 onNext={handleNextStep} />}
       {step === 2 && <Step2 />}
