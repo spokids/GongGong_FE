3c8d5fce1847d731555b199c038d45a506d08846
Sohyunnnn <thgus05061@naver.com>
2024-11-22 21:19:25 +0900

    #11 [Feat] 조건 찾기 페이지 퍼블리싱

---
+++ src/pages/ProgramFinder/index.tsx
@@ -0,0 +1,56 @@
+import Input from "@components/Input";
+import FieldButton from "./components/FieldButton";
+import { SwimmingIcon } from "@assets/svg";
+import Button from "@components/Button";
+
+const ProgramFinder = () => {
+  return (
+    <div>
+      <div className="mt-3 flex flex-col gap-1">
+        <h2 className="text-primary-100 text-title1 font-semibold">
+          지역, 분야, 아이의 나이를 <br />
+          입력하고 잘 맞는 프로그램을 찾아봐요!
+        </h2>
+        <h2 className="text-body7 font-regular text-primary-40">
+          3개의 항목 중 한가지만 입력해도 적용할 수 있어요
+        </h2>
+      </div>
+      <div className="mt-10">
+        <div className="flex gap-3 items-center mb-1">
+          <p className="text-body6 font-medium text-primary-100">지역</p>
+          <p className="text-caption4 font-regular text-orange-400">
+            시 / 도까지 필수입력
+          </p>
+        </div>
+      </div>
+      <div className="mt-10">
+        <div className="flex gap-3 items-center mb-1">
+          <p className="text-body6 font-medium text-primary-100">분야</p>
+          <p className="text-caption4 font-regular text-orange-400">
+            여러 개 선택할 수 있어요
+          </p>
+        </div>
+        <FieldButton icon={SwimmingIcon} label="수영" />
+      </div>
+      <div className="mt-[42px]">
+        <div className="flex flex-col">
+          <p className="text-primary-100 text-body6 font-medium">아이의 나이</p>
+          <p className="text-caption4 text-primary-40 font-regular">
+            나이는 최대 만 18세까지 입력 가능해요.
+          </p>
+        </div>
+        <div className="flex items-center">
+          <p className="text-body6 font-medium text-primary-100 mr-5">만</p>
+          <Input className="w-[294px]" placeholder="숫자로 입력해주세요" />
+          <p className="ml-2">세</p>
+        </div>
+        <Button
+          className="mt-[136px] flex bg-[#FF8C2E] text-white"
+          text="다음"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default ProgramFinder;
