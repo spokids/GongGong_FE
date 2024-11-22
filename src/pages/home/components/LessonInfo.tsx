13fbb5f81d9a8ce3ac54050b2eb7c719d9a9c3c2
yena0213 <yeana3131@naver.com>
2024-11-23 07:38:54 +0900

    #13 [Design] info 컴포넌트 속성 보완

--- src/pages/home/components/LessonInfo.tsx
+++ src/pages/home/components/LessonInfo.tsx
@@ -11,12 +11,18 @@ interface LessonInfoProps {
   programDate: string;
 }
 
-const InfoBox: React.FC<{ title: string; value: string }> = ({ title, value }) => (
-  <div className="flex flex-col">
-    <p className="text-button3 text-primary-60">{title}</p>
-    <p className="text-body8 text-primary-80">{value}</p>
-  </div>
-);
+const InfoBox: React.FC<{ title: string; value: string; className?: string }> = ({ title, value, className }) => {
+    return (
+      <div className={`flex flex-col ${className} w-full max-w-[120px]`}>
+        <p className="text-button3 text-primary-60">{title}</p>
+        <p className="overflow-hidden truncate text-body9 text-primary-80 whitespace-nowrap">
+          {value}
+        </p>
+      </div>
+    );
+  };
+  
+  
 
 const LessonInfo: React.FC<LessonInfoProps> = ({
   programType,
@@ -26,19 +32,19 @@ const LessonInfo: React.FC<LessonInfoProps> = ({
   programDate,
 }) => {
   return (
-    <>
+    <div className="mt-4 mb-4">
       <Chip>
         <img src={swimming} alt="Swimming" className="w-4 h-4 mr-2" />
         {programType}
       </Chip>
 
       <h3 className="mt-2 text-body6">{programName}</h3>
-      <div className="flex flex-row w-full h-14 mt-3 items-center gap-3 p-2 rounded-lg bg-[#F7F7F7]">
-        <InfoBox title="기관명" value={facilityName} />
+      <div className="flex flex-row w-full h-14 mt-3 items-center justify-between p-2 rounded-lg bg-[#F7F7F7]">
+      <InfoBox title="기관명" value={facilityName} />
         <InfoBox title="프로그램 대상" value={programAge} />
         <InfoBox title="프로그램 기간" value={programDate} />
       </div>
-    </>
+    </div>
   );
 };
 
