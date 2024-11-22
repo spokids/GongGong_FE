fc5b9a0fc507d03605b977d6e12b2dacdfb86503
yena0213 <yeana3131@naver.com>
2024-11-23 06:47:57 +0900

    #13 [Chore] App.tsx 코드 변경

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
-import Home from "@pages/HomePage";
 import React from "react";
 import { Outlet } from "react-router-dom";
 
 const App: React.FC = () => {
   return (
     <div className="container max-w-[410px] min-w-[390px] mx-auto flex">
-      <Home />
+      <Outlet />
     </div>
   );
 };
