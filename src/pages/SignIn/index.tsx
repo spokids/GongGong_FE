fe0b0c6b1de434f0f2b3911b2b7833ab20db88bf
Sohyunnnn <thgus05061@naver.com>
2024-11-22 19:27:09 +0900

    #9 [Chrore] 디렉토리 구조 변경

---
+++ src/pages/SignIn/index.tsx
@@ -0,0 +1,51 @@
+import { Link } from "react-router-dom";
+import Button from "../../components/Button";
+import Input from "../../components/Input";
+import BgLogo from "../../assets/svg/BgLogo";
+import LogoLogin from "../../assets/svg/LogoLogin";
+
+const SignIn = () => {
+  return (
+    <div className="flex flex-col items-center mt-[94px] pace-y-6">
+      <div className="mb-[120px] relative w-full h-[300px]">
+        <BgLogo className="absolute top-0 left-0 object-cover w-full h-full" />
+        <LogoLogin className="absolute top-[60%] left-[50%] transform -translate-x-1/2 -translate-y-1/2 w-[180px]" />
+      </div>
+
+      <div className="flex flex-col w-[350px] gap-2 mb-[70px]">
+        <Input placeholder="아이디를 입력해주세요." />
+        <Input type="password" placeholder="비밀번호를 입력해주세요." />
+      </div>
+
+      <Button className="flex bg-[#FF8C2E] text-white" text="로그인하기" />
+
+      <text
+        style={{
+          display: "flex",
+          color: "#484851",
+          fontSize: "14px",
+          letterSpacing: "-0.42px",
+          marginTop: "20px",
+        }}
+      >
+        스포키즈가 처음이신가요?
+      </text>
+      <Link to="/signup">
+        <text
+          style={{
+            display: "flex",
+            color: "#484851",
+            fontSize: "14px",
+            letterSpacing: "-0.42px",
+            textDecorationLine: "underline",
+            marginTop: "-20px",
+          }}
+        >
+          회원가입하기
+        </text>
+      </Link>
+    </div>
+  );
+};
+
+export default SignIn;
--- src/pages/SignUp/Step2/index.tsx
+++ src/pages/SignUp/Step2/index.tsx
@@ -1,5 +1,5 @@
-import Button from "../../components/Button";
-import Input from "../../components/Input";
+import Button from "../../../components/Button";
+import Input from "../../../components/Input";
 
 const Step2 = () => {
   return (
