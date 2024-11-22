bcf603d7529c8b53a46bdb489e77ed1ab5e8ebe3
Sohyunnnn <thgus05061@naver.com>
2024-11-22 19:24:08 +0900

    #9 [Chore] add types/node

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,36 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import svgr from 'vite-plugin-svgr';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import svgr from "vite-plugin-svgr";
+import path from "path";
 
 export default defineConfig({
   plugins: [
     react(),
     svgr({
       svgrOptions: {
-        icon: true, 
+        icon: true,
       },
     }),
   ],
-  root: './',
+  root: "./",
   build: {
     rollupOptions: {
-      input: '/src/main.tsx',
+      input: "/src/main.tsx",
+    },
+  },
+  resolve: {
+    alias: {
+      "@pages": path.resolve(__dirname, "src/pages"),
+      "@types": path.resolve(__dirname, "src/types"),
+      "@api": path.resolve(__dirname, "src/api"),
+      "@constants": path.resolve(__dirname, "src/constants"),
+      "@utils": path.resolve(__dirname, "src/utils"),
+      "@lib": path.resolve(__dirname, "src/lib"),
+      "@styles": path.resolve(__dirname, "src/styles"),
+      "@components": path.resolve(__dirname, "src/components"),
+      "@hooks": path.resolve(__dirname, "src/hooks"),
+      "@assets": path.resolve(__dirname, "src/assets"),
+      "@features": path.resolve(__dirname, "src/features"),
     },
   },
 });
