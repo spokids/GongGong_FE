4a24d348188b6e7c27dd3693de34366c718c115c
yena0213 <yeana3131@naver.com>
2024-11-22 23:09:43 +0900

    #13 [Chore] mainIcon 로고 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
+import Home from "@pages/Home";
 import React from "react";
 import { Outlet } from "react-router-dom";
 
 const App: React.FC = () => {
   return (
     <div className="container max-w-[410px] min-w-[390px] mx-auto flex">
-      <Outlet />
+      <Home/>
     </div>
   );
 };
