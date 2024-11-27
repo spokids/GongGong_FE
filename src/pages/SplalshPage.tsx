fe0f4ce5f97c4a7b8e4a5c51d68990dba5e727e2
Sohyunnnn <thgus05061@naver.com>
2024-11-27 22:50:03 +0900

    chore: 기타 코드 작성

--- src/pages/SplalshPage.tsx
+++ src/pages/SplalshPage.tsx
@@ -1,11 +1,7 @@
-import React from 'react';
+import React from "react";
 
 const SplalshPage = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  return <div>SplalshPage</div>;
 };
 
 export default SplalshPage;
