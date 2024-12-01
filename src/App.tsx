6581eb10d3882bff57063208280a66093a899627
Sohyunnnn <thgus05061@naver.com>
2024-12-02 00:43:53 +0900

    #25 [Design] 디자인 수정

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { Outlet } from "react-router-dom";
 
 const App: React.FC = () => {
   return (
-    <div className="container max-w-[410px] min-w-[390px] mx-auto flex flex-col">
+    <div className="container max-w-[410px] min-w-[390px] mx-auto flex flex-col h-[100vh]">
       <Header />
       <Outlet />
     </div>
