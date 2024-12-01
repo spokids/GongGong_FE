57b31a801e0db288da250a2700d2b134edc90bd4
Sohyunnnn <thgus05061@naver.com>
2024-12-02 01:12:42 +0900

    #25 [Feat] 챗봇 퍼블리싱

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,6 +15,7 @@ export default {
       container: {
         center: true,
         padding: "20px",
+        height: "100%",
       },
       colors: {
         system: {
@@ -79,6 +80,9 @@ export default {
         regular: "400",
       },
     },
+    backgroundImage: {
+      "linear-orange":
+        "linear-gradient(180deg, #FFF 24.64%, #FFF8F2 72.39%, #FFDEC2 113.93%, #FF8C2E 129.38%)",
+    },
   },
-  plugins: [],
 };
