b17e469fefa296e20ee3c8ee8141295bb5e77c72
Sohyunnnn <thgus05061@naver.com>
2024-12-01 21:04:17 +0900

    #21 [Chore] svgr 문제 해결

--- src/assets/svg/index.ts
+++ src/assets/svg/index.ts
@@ -2,6 +2,4 @@
 // export { default as LogoLogin } from "./LogoLogin";
 // export { default as SwimmingIcon } from "./swimming.svg";
 
-
-
 export { default as SwimmingIcon } from "./swimming.svg?react";
--- vite.config.ts
+++ vite.config.ts
@@ -1,39 +1,20 @@
 import { defineConfig } from "vite";
-import tsConfigPaths from "vite-tsconfig-paths";
 import react from "@vitejs/plugin-react";
+import tsconfigPaths from "vite-tsconfig-paths";
 import svgr from "vite-plugin-svgr";
-import path from "path";
 
 export default defineConfig({
   plugins: [
     react(),
-    tsConfigPaths(),
+    tsconfigPaths(),
     svgr({
-      include: "**/*.svg",
-      svgrOptions: {
-        icon: true,
-      },
+      include: "**/*.svg?react",
     }),
   ],
   root: "./",
   build: {
     rollupOptions: {
       input: "/src/main.tsx",
-    },
-  },
-  resolve: {
-    alias: {
-      "@pages": path.resolve(__dirname, "src/pages"),
-      "@types": path.resolve(__dirname, "src/types"),
-      "@api": path.resolve(__dirname, "src/api"),
-      "@constants": path.resolve(__dirname, "src/constants"),
-      "@utils": path.resolve(__dirname, "src/utils"),
-      "@lib": path.resolve(__dirname, "src/lib"),
-      "@styles": path.resolve(__dirname, "src/styles"),
-      "@components": path.resolve(__dirname, "src/components"),
-      "@hooks": path.resolve(__dirname, "src/hooks"),
-      "@assets": path.resolve(__dirname, "src/assets"),
-      "@features": path.resolve(__dirname, "src/features"),
     },
   },
 });
