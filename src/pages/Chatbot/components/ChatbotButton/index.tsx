e1053134a61ad0f61f5ef4cf5d3e83d48b8a9fc8
Sohyunnnn <thgus05061@naver.com>
2024-12-03 15:43:11 +0900

    #30 [Feat] FieldButton 클릭 시 ChatbotButton 활성화 되는 로직 구현

--- src/pages/Chatbot/components/ChatbotButton/index.tsx
+++ src/pages/Chatbot/components/ChatbotButton/index.tsx
@@ -1,14 +1,22 @@
 interface ChatbotButtonProps {
   children: React.ReactNode;
   onClick?: () => void;
+  disabled?: boolean;
 }
 
-const ChatbotButton: React.FC<ChatbotButtonProps> = ({ children, onClick }) => {
+const ChatbotButton: React.FC<ChatbotButtonProps> = ({
+  children,
+  onClick,
+  disabled,
+}) => {
   return (
     <button
       type="button"
-      className="flex rounded-xl bg-primary-100 px-4 py-3 text-button2 font-semibold text-white"
-      onClick={onClick}
+      className={`flex rounded-xl bg-primary-100 px-4 py-3 text-button2 font-semibold ${
+        disabled ? "cursor-not-allowed text-primary-70" : "text-white"
+      }`}
+      onClick={disabled ? undefined : onClick}
+      disabled={disabled}
     >
       {children}
     </button>
