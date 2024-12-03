da1c42073f9a41aab3922b9cd1284f4d75c2cbb1
Sohyunnnn <thgus05061@naver.com>
2024-12-03 14:27:26 +0900

    #30 [Feat] SenderBubble message props 처리

--- src/pages/Chatbot/components/SenderBubble/index.tsx
+++ src/pages/Chatbot/components/SenderBubble/index.tsx
@@ -1,7 +1,11 @@
-const SenderBubble = () => {
+interface SenderBubbleProps {
+  message: string;
+}
+
+const SenderBubble = ({ message }: SenderBubbleProps) => {
   return (
     <div className="mt-6 flex-shrink-0 rounded-bl-xl rounded-br-xl rounded-tl-xl rounded-tr-none border-[1.5px] border-orange-100 p-3 text-body8 font-medium text-orange-400">
-      하이
+      {message}
     </div>
   );
 };
