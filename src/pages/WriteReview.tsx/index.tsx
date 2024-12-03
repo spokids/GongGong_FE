541c692b6cfbe1c0522e3bab1c0f155b648fc3de
yena0213 <yeana3131@naver.com>
2024-12-04 05:49:40 +0900

    #26 [Feat] 이미지 삽입 기능 추가

--- src/pages/WriteReview.tsx/index.tsx
+++ src/pages/WriteReview.tsx/index.tsx
@@ -1,7 +1,9 @@
 import { useState } from "react";
 import { PictureIcon } from "@assets/svg";
+import React from "react";
 
 const WriteReview = () => {
+    const [imgCnt, setImgCnt] = useState<number>(0);
     const [selectedImage, setSelectedImage] = useState<File | null>(null);
     const [text, setText] = useState<string>(""); 
     const [isWriting, setIsWriting] = useState(false);
@@ -17,6 +19,20 @@ const WriteReview = () => {
 
     const handleBlur = () => {
         setIsWriting(false);
+    };
+    const triggerFileInput = () => {
+        if (fileInputRef.current) {
+            fileInputRef.current.click();
+        }
+    };
+
+    const fileInputRef = React.useRef<HTMLInputElement | null>(null);
+
+    const handleImageUpload = (event: React.ChangeEvent<HTMLInputElement>) => {
+        if (event.target.files && event.target.files.length > 0) {
+            setSelectedImage(event.target.files[0]);
+            setImgCnt(1);
+        }
     };
 
     return (
@@ -49,10 +65,44 @@ const WriteReview = () => {
                 <span className='text-primary-foundation-50 text-caption4'>선택</span>
             </div>
 
-            <div className='w-[100px] mt-2 h-[100px] flex justify-center items-center border-[1.5px] rounded-lg bg-foundation-5'>
-                <PictureIcon />
+            <div className="flex flex-row gap-2 mt-2">
+                {selectedImage ? (
+                    <>
+                    <div className="w-[100px] h-[100px] mt-2 flex justify-center items-center rounded-lg bg-primary-5">
+                        <PictureIcon style={{ fill: "#D1D1D3" }} />
+                     </div>
+                    <div className="w-[100px] h-[100px] mt-2 flex justify-center items-center border-[1.5px] rounded-lg overflow-hidden">
+                        <img
+                            src={URL.createObjectURL(selectedImage)}
+                            alt="Uploaded"
+                            className="object-cover w-full h-full"
+                        />
+                    </div>
+                    </>
+                ) : (
+                    <button
+                    onClick={triggerFileInput}
+                    className="w-[100px] h-[100px] mt-2 flex justify-center items-center border-[2px] rounded-lg bg-primary-5 border-primary-10"
+                    >
+                    <PictureIcon/>
+                    </button>
+                )}
+                <input
+                    type="file"
+                    ref={fileInputRef}
+                    className="hidden"
+                    onChange={handleImageUpload}
+                />
             </div>
-            
+
+            <div className='mt-1 text-caption4 text-primary-40 ml-[39px]'>
+                <span className={imgCnt ? 'text-primary-60' : ''}>
+                    {imgCnt}
+                </span>
+                <span> / 1 </span>
+            </div>
+
+
             <h3 className='text-primary-70 mt-[51px] text-caption3'>쾌적한 스포키즈를 위해 꼭 지켜주세요!</h3>
             <p className='text-primary-50 text-caption4'>
                 욕설, 초상권 침해, 비방 및 허위 사실 등을 포함한 후기는 삭제 될 수 있으며
