0e362927c87592dd5a6f6ca80fdf93d9426e6ca6
Sohyunnnn <thgus05061@naver.com>
2024-12-03 13:52:34 +0900

    #30 [Feat] SenderBubble 퍼블리싱

---
+++ src/pages/Chatbot/components/SenderBubble/index.tsx
@@ -0,0 +1,9 @@
+const SenderBubble = () => {
+  return (
+    <div className="mt-6 flex-shrink-0 rounded-bl-xl rounded-br-xl rounded-tl-xl rounded-tr-none border-[1.5px] border-orange-100 p-3 text-body8 font-medium text-orange-400">
+      하이
+    </div>
+  );
+};
+
+export default SenderBubble;
