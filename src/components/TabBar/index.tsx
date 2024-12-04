94784c295f012a4876b0acdc419cf61e4d9462aa
yena0213 <yeana3131@naver.com>
2024-12-04 22:33:47 +0900

    #38 [chore] components from 30'

--- src/components/TabBar/index.tsx
+++ src/components/TabBar/index.tsx
@@ -4,13 +4,13 @@ import Button from "@components/Button";
 const TabBar = () => {
   return (
     <div className="flex justify-center py-[9px] gap-3">
-      <Button className="flex gap-2 py-2 px-11">
+      <Button className="py-2 px-11 flex gap-2">
         <HomeIcon />
-        <p className="font-normal text-caption2">홈</p>
+        <p className="text-caption2 font-normal">홈</p>
       </Button>
       <Button style="filled" className="px-6 py-2 rounded-lg">
         <MessageIcon />
-        <p className="font-normal text-caption2">스포키톡</p>
+        <p className="text-caption2 font-normal">스포키톡</p>
       </Button>
     </div>
   );
