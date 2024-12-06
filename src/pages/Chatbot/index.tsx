b228e60019c5bb32f21e8d2ddcb7455cb908633b
Sohyunnnn <thgus05061@naver.com>
2024-12-07 02:27:00 +0900

    #49 [Refactor] ability 페이지 분리

--- src/pages/Chatbot/index.tsx
+++ src/pages/Chatbot/index.tsx
@@ -1,83 +1,25 @@
-import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import BotBubble from "../../components/BotBubble";
+import ChatbotButton from "../../components/ChatbotButton";
+import routes from "constants/routes";
 import usePostChoiceChatRoom from "@api/hooks/chatbot/usePostChoiceChatRoom";
-import FreeChat from "./components/FreeChat";
-import AbilityChat from "./components/AbilityChat";
-import BotBubble from "./components/BotBubble";
-import ChatbotButton from "./components/ChatbotButton";
-import ChatbotInput from "./components/ChatbotInput";
-import usePostAbility from "@api/hooks/chatbot/usePostAbility";
-import { Program } from "@api/types/chatbot";
-
 
 const Chatbot = () => {
+  const navigate = useNavigate();
   const { mutate: postChoiceChatRoom } = usePostChoiceChatRoom();
-  const [buttonClicked, setButtonClicked] = useState(false);
-  const [choice, setChoice] = useState<string>("");
-  const [chatRoomId, setChatRoomId] = useState<number | null>(null);
-  const [showChatbotInput, setShowChatbotInput] = useState(false);
-  const { mutate: postAbility } = usePostAbility();
-  const [programs, setPrograms] = useState<Program[]>([]);
-  const [region, setRegion] = useState<string | null>(null);
-  const [totalPages, setTotalPages] = useState<number | undefined>(undefined);
-  const [currentPage, setCurrentPage] = useState<number>(1);
-
 
   const handleButtonClick = (choice: string) => {
-    setChoice(choice);
-    postChoiceChatRoom(choice, { 
+    postChoiceChatRoom(choice, {
       onSuccess: (response) => {
         if (response.data) {
           const chatRoomId = response.data.chatRoomId;
-          setChatRoomId(chatRoomId);
+
+          navigate(choice === "FREE_CHAT" ? routes.Free : routes.Ability, {
+            state: { chatRoomId },
+          });
         }
-        setButtonClicked(true);
       },
     });
-  };
-
-  const handleInputButtonClick = (region: string) => {
-    setRegion(region);
-    if (choice === "ABILITY_CHAT" && chatRoomId) {
-      setShowChatbotInput(false);
-      const abilities = null;
-      postAbility(
-        { chatRoomId, abilities, region },
-        {
-          onSuccess: (response) => {
-            if (response.data && response.data.isSuccess) {
-              setPrograms(response.data.programs); 
-              setTotalPages(response.data.totalPage);
-            }
-          },
-        }
-      );
-    }
-  };
-
-  const handleReset = () => {
-    setButtonClicked(false);
-    setRegion(null);
-    setPrograms([]);
-    setShowChatbotInput(false);
-    setChatRoomId(null);
-    setChoice("");
-  };
-  
-  const handlePageChange = (page: number) => {
-    setCurrentPage(page);
-    const abilities = null;
-    if(chatRoomId){
-      postAbility(
-        { chatRoomId, abilities, region, page },
-        {
-          onSuccess: (response) => {
-            if (response.data && response.data.isSuccess) {
-              setPrograms(response.data.programs); 
-            }
-          },
-        }
-      );
-    }
   };
 
   return (
@@ -87,36 +29,16 @@ const Chatbot = () => {
           찾아주는 스포키톡이에요!
           프로그램을 찾기 위한 기준을 아래에서 선택해주세요.`}
       />
-      {buttonClicked && choice === "FREE_CHAT" && <FreeChat />}
-      {buttonClicked && choice === "ABILITY_CHAT" && chatRoomId && (
-        <AbilityChat
-        chatRoomId={chatRoomId}
-        setShowChatbotInput={setShowChatbotInput}
-        programs={programs}
-        region={region}
-        onReset={handleReset}
-        totalPages={totalPages}
-        currentPage={currentPage}
-        onPageChange={handlePageChange}
-      />
-      )}
-      {!buttonClicked && (
-        <div className="mb-6 mt-auto flex flex-col justify-center gap-2 px-[34px]">
-          <>
-            <ChatbotButton onClick={() => handleButtonClick("FREE_CHAT")}>
-              자유롭게 아이에게 맞는 프로그램을 찾고싶어요.
-            </ChatbotButton>
-            <ChatbotButton onClick={() => handleButtonClick("ABILITY_CHAT")}>
-              키우고 싶은 능력치를 기준으로 찾고 싶어요.
-            </ChatbotButton>
-          </>
-        </div>
-      )}
-      {showChatbotInput && (
-        <div className="mt-auto">
-          <ChatbotInput onClick={handleInputButtonClick} />
-        </div>
-      )}
+      <div className="mb-6 mt-auto flex flex-col justify-center gap-2 px-[34px]">
+        <>
+          <ChatbotButton onClick={() => handleButtonClick("FREE_CHAT")}>
+            자유롭게 아이에게 맞는 프로그램을 찾고싶어요.
+          </ChatbotButton>
+          <ChatbotButton onClick={() => handleButtonClick("ABILITY_CHAT")}>
+            키우고 싶은 능력치를 기준으로 찾고 싶어요.
+          </ChatbotButton>
+        </>
+      </div>
     </div>
   );
 };
