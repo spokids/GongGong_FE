94b386431417d2c3b5866db3d0ea237305a180d4
yena0213 <yeana3131@naver.com>
2024-12-07 03:27:10 +0900

    #46 [feat] programFinder API create

--- src/api/programAPI.ts
+++ src/api/programAPI.ts
@@ -1,7 +1,7 @@
 // src/api/programAPI.ts
 
-import { apiGet,  } from "./apiUtil";
-import { DongResponse, SigunguResponse } from "@api/types/program";
+import { apiGet, apiPost } from "./apiUtil";
+import { DongResponse, ProgramRequest, ProgramResponse, SigunguResponse } from "@api/types/program";
 
 // 2.1 시/군/구 조회
 export const getSigungu = (province: string) => {
@@ -11,4 +11,9 @@ export const getSigungu = (province: string) => {
 // 2.2 동/읍/면/리 조회
 export const getDong = (province: string, sigungu: string) => {
   return apiGet<DongResponse>(`/program/dong`, {province, sigungu});
+}
+
+// 2.3 프로그램 조회
+export const postProgram = (data: ProgramRequest, page: number, size: number) => {
+  return apiPost<ProgramResponse>(`/program?page=${page}&size=${size}`, data);
 }
--- src/api/types/program.ts
+++ src/api/types/program.ts
@@ -9,3 +9,32 @@ export interface SigunguResponse {
 export interface DongResponse {
   dong: string[];
 }
+
+// 2.3
+export interface ProgramRequest {
+  province: string;
+  sigungu: string;
+  dong?: string;
+  age?: number;
+  types?: string[];
+}
+
+// 2.3
+export interface ProgramResponse {
+  programList: {
+    programId: number;
+    programType: string;
+    programName: string;
+    facilityName: string;
+    pargarmAge: string;
+    programDate: string;
+  }[];
+  conditionList: {
+    location: string;
+    age: string | null;
+    existingTypes: string[] | null;
+    notExistingTypes: string[] | null;
+  }[];
+  totalPage: number;
+  currentPage: number;
+}
