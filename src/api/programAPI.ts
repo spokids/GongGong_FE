5f75b0e4a47c79e3d05dabdfc259a039c3a4a107
yena0213 <yeana3131@naver.com>
2024-12-08 01:38:04 +0900

    #46 [fix] main merge- finder

--- src/api/programAPI.ts
+++ src/api/programAPI.ts
@@ -7,7 +7,16 @@ export const getSigungu = (province: string) => {
   return apiGet<SigunguResponse>(`/program/sigungu?province=${province}`);
 }
 
+<<<<<<< HEAD
 export const getDong = (sigungu: string, province: string) => {
+=======
+// 2.2 동/읍/면/리 조회
+export const getDong = (sigungu: string, province: string) => {
+  console.log("전달된 province:", province);
+  console.log("전달된 sigungu:", sigungu);
+  console.log("완전한 URL:", `/program/dong?province=${province}&sigungu=${sigungu}`);
+  
+>>>>>>> c4fdecd3 (#46 [feat] 동읍면리 api 연동 완료)
   return apiGet<DongResponse>(`/program/dong?province=${province}&sigungu=${sigungu}`);
 }
 
