29f0c2fb34bc67389c06f50ef5d68ce0711def47
Sohyunnnn <thgus05061@naver.com>
2024-12-07 12:57:18 +0900

    #52 [Feat] 회원가입 api 연동

--- src/pages/SignUp/Step1/index.tsx
+++ src/pages/SignUp/Step1/index.tsx
@@ -1,27 +1,61 @@
 import Input from "@components/Input";
 import Button from "../../../components/Button";
+import { useFormContext } from "react-hook-form";
 
 interface Step1Props {
   onNext: () => void;
 }
 
-const Step1 = ({ onNext }: Step1Props) => {
+const Step1: React.FC<Step1Props> = ({ onNext }) => {
+  const {
+    register,
+    watch,
+    formState: { isValid },
+  } = useFormContext();
+  
+
+  const [userInputId, password, confirmPassword] = watch([
+    "userInputId",
+    "password",
+    "confirmPassword",
+  ]);
+
+  const isPasswordValid = password === confirmPassword;
+
   return (
     <>
       <div className="flex flex-col gap-1 mt-10">
         <label className="text-primary-100 text-body6 font-medium">아이디</label>
-        <Input placeholder="사용하실 아이디를 입력해주세요" />
+        <Input
+          placeholder="사용하실 아이디를 입력해주세요"
+          {...register('userInputId', { required: true })}
+        />
       </div>
+
       <div className="flex flex-col gap-1 mt-10">
-      <label className="text-primary-100 text-body6 font-medium">비밀번호</label>
-        <Input type="password" placeholder="비밀번호를 입력해주세요" />
+        <label className="text-primary-100 text-body6 font-medium">비밀번호</label>
+        <Input
+          type="password"
+          placeholder="비밀번호를 입력해주세요"
+          {...register('password', { required: true })}
+        />
       </div>
+
       <div className="flex flex-col gap-1 mt-5">
-      <label className="text-primary-100 text-body6 font-medium">비밀번호 확인</label>
-        <Input type="password" placeholder="비밀번호를 다시 입력해주세요" />
+        <label className="text-primary-100 text-body6 font-medium">비밀번호 확인</label>
+        <Input
+          type="password"
+          placeholder="비밀번호를 다시 입력해주세요"
+          {...register('confirmPassword', {
+            required: true,
+          })}
+        />
       </div>
+
       <Button
         className="mt-[136px] flex bg-orange-400 text-white w-full h-14"
+        type="submit"
+        disabled={!isValid || !isPasswordValid}
         onClick={onNext}
       >
         다음
--- src/pages/SignUp/Step2/index.tsx
+++ src/pages/SignUp/Step2/index.tsx
@@ -1,16 +1,34 @@
 import Input from "@components/Input";
 import Button from "../../../components/Button";
+import { useFormContext } from "react-hook-form";
 
-const Step2 = () => {
+const Step2: React.FC = () => {
+  const { register, watch } = useFormContext();
+  
+  const nickname = watch("nickName", "");
+
+  const isFormValid = nickname.length > 0 && nickname.length <= 8;
+
   return (
     <>
       <div className="flex flex-col gap-1 mt-10">
         <label className="text-primary-100 text-body6 font-medium">닉네임</label>
-        <Input />
+        <Input
+          {...register("nickName", { 
+            required: "닉네임은 필수입니다.", 
+            maxLength: { value: 8, message: "8자 이하로 입력해주세요." } 
+          })}
+        />
       </div>
-      <p className="mt-[6px] text-primary-40 text-caption4">8자 이내의 한글/영문/숫자로 입력해주세요.</p>
-      <Button className="mt-[366px] flex bg-orange-400 text-white w-full h-14">
-        다음
+      <p className="mt-[6px] text-primary-40 text-caption4">
+        8자 이내의 한글/영문/숫자로 입력해주세요.
+      </p>
+      <Button
+        type="submit"
+        className="mt-[366px] flex bg-orange-400 text-white w-full h-14"
+        disabled={!isFormValid}
+      >
+        완료
       </Button>
     </>
   );
