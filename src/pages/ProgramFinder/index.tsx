59dc1db830bf487a08f90fd5e12603f7017db59c
yena0213 <yeana3131@naver.com>
2024-12-08 02:04:48 +0900

    #46 [fix] main merge-5

--- src/pages/ProgramFinder/index.tsx
+++ src/pages/ProgramFinder/index.tsx
@@ -1,114 +1,21 @@
-import { BadmintonIcon, BasketballIcon, BowlingIcon, DancinggIcon, DumbellsIcon, EllipicalIcon, FencingIcon, FitnessIcon, GolfIcon, HollaHoopgIcon, JumpRoopIcon, MoreCircleIcon, PiaonIcon, RollerStakeIcon, SoccerIcon, SwimmingIcon, TableTennisIcon, TennisIcon, VolleyballIcon, WarriorIcon } from "@assets/svg";
+import { SwimmingIcon } from "@assets/svg";
 import FieldButton from "@components/FieldButton";
 import RegionDropdown from "./components/RegionDropdown";
 import Button from "@components/Button";
 import Input from "@components/Input";
 import { useGetSigungu } from "@api/hooks/program/useGetSigungu";
-<<<<<<< HEAD
-<<<<<<< HEAD
 import { useGetDong } from "@api/hooks/program/useGetdong";
-import { useEffect, useState } from "react";
-
-const ProgramFinder = () => {
-<<<<<<< HEAD
-  const [isRegionSelected, setRegionSelected] = useState(false);
-  const [isSigunguSelected, setSigunguSelected] = useState(false);
-  const [selectedRegion, setSelectedRegion] = useState<string>("서울특별시");
-  const [selectedState, setSelectedState] = useState<string>("");
-  const [selectedDong, setSelectedDong] = useState(false);
-
-  const { data: sigunguData, isError: sigunguError } = useGetSigungu(selectedRegion);
-  const { data: dongData, isError: dongError } = useGetDong(selectedState, selectedRegion);
-
-
-  const states = sigunguError ? ["정보없음"] : sigunguData || [];
-  const dongs = dongError ? ["정보없음"] : dongData || [];
-
-  const handleRegionSelect = (region: string) => {
-    setSelectedRegion(region);
-    setRegionSelected(true);
-    setSigunguSelected(false);
-    setSelectedState("");
-    setSelectedDong(false);
-  };
-
-  const handleStateSelect = (states: string) => {
-    setSelectedDong(false);
-    setSigunguSelected(true);
-    setSelectedState(states);
-  };
-
-  const handleDongSelect = () => {
-    setSelectedDong(true);
-  };
-
-  useEffect(() => {
-    setSelectedDong(false);
-  }, [selectedState]);
-
-  const regions = [
-    "서울특별시",
-    "경상북도",
-    "경기도",
-    "강원특별자치도",
-    "부산광역시",
-    "대구광역시",
-    "인천광역시",
-    "충청남도",
-    "경상남도",
-    "충청북도",
-    ];
-  const fields = [
-    { icon: <SwimmingIcon />, label: "수영" },
-    { icon: <BadmintonIcon />, label: "배드민턴" },
-    { icon: <FitnessIcon />, label: "에어로빅" },
-    { icon: <BasketballIcon />, label: "농구" },
-    { icon: <DumbellsIcon />, label: "헬스" },
-    { icon: <TableTennisIcon />, label: "탁구" },
-    { icon: <RollerStakeIcon />, label: "인라인" },
-    { icon: <DancinggIcon />, label: "댄스" },
-    { icon: <GolfIcon />, label: "골프" },
-    { icon: <HollaHoopgIcon />, label: "무용" },
-    { icon: <SoccerIcon />, label: "축구" },
-    { icon: <FencingIcon />, label: "검도" },
-    { icon: <VolleyballIcon />, label: "배구" },
-    { icon: <BowlingIcon />, label: "볼링" },
-    { icon: <EllipicalIcon />, label: "스피닝" },
-    { icon: <TennisIcon />, label: "스쿼시" },
-    { icon: <WarriorIcon />, label: "자세교정" },
-    { icon: <TennisIcon />, label: "테니스" },
-    { icon: <PiaonIcon />, label: "피아노" },
-    { icon: <JumpRoopIcon />, label: "음악줄넘기" },
-    { icon: <MoreCircleIcon />, label: "기타" },
-  ];
-=======
-=======
-=======
-import { useGetDong } from "@api/hooks/program/useGetdong";
->>>>>>> c4fdecd3 (#46 [feat] 동읍면리 api 연동 완료)
 import { useState } from "react";
 
 const ProgramFinder = () => {
   const [isSelected, setSelected] = useState(false);
   const [isSigunguSelected, setSigunguSelected] = useState(false);
-<<<<<<< HEAD
->>>>>>> 1aa83d92 (#46 [fix] main merge- finder)
-  const [selectedRegion, setSelectedRegion] = useState<string>("서울특별시"); 
-  const { data } = useGetSigungu(selectedRegion);
-=======
   const [selectedRegion, setSelectedRegion] = useState<string>("서울특별시");
   const [selectedState, setSelectedState] = useState<string>("");
-<<<<<<< HEAD
-  
-  const { data: sigunguData } = useGetSigungu(selectedRegion);
-  const { data: dongData } = useGetDong(selectedState, selectedRegion);
->>>>>>> c4fdecd3 (#46 [feat] 동읍면리 api 연동 완료)
-=======
   const [selectedDong, setSelectedDong] = useState(false);
 
   const { data: sigunguData, isError: sigunguError } = useGetSigungu(selectedRegion);
   const { data: dongData, isError: dongError } = useGetDong(selectedState, selectedRegion);
->>>>>>> 83414352 (#46 [feat] 버튼 비활성화 설정)
 
   const handleRegionSelect = (region: string) => {
     setSelectedRegion(region);
@@ -116,11 +23,6 @@ const ProgramFinder = () => {
     setSigunguSelected(false);
     setSelectedState("");
   };
-<<<<<<< HEAD
-  
-  const regions = data || [];
->>>>>>> 31954a58 (#46 [fix] main merge)
-=======
 
   const handleStateSelect = (states: string) => {
     setSelectedState(states);
@@ -144,42 +46,8 @@ const ProgramFinder = () => {
     "충청북도",
   ];
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-  const states = data || [];
->>>>>>> 1aa83d92 (#46 [fix] main merge- finder)
-=======
-  const states = sigunguData || [];
-  const dongs = dongData || [];
->>>>>>> c4fdecd3 (#46 [feat] 동읍면리 api 연동 완료)
-=======
   const states = sigunguError ? ["정보없음"] : sigunguData || [];
   const dongs = dongError ? ["정보없음"] : dongData || [];
->>>>>>> 83414352 (#46 [feat] 버튼 비활성화 설정)
-
-  const fields = [
-    { icon: <SwimmingIcon />, label: "수영" },
-    { icon: <BadmintonIcon />, label: "배드민턴" },
-    { icon: <FitnessIcon />, label: "에어로빅" },
-    { icon: <BasketballIcon />, label: "농구" },
-    { icon: <DumbellsIcon />, label: "헬스" },
-    { icon: <TableTennisIcon />, label: "탁구" },
-    { icon: <RollerStakeIcon />, label: "인라인" },
-    { icon: <DancinggIcon />, label: "댄스" },
-    { icon: <GolfIcon />, label: "골프" },
-    { icon: <HollaHoopgIcon />, label: "무용" },
-    { icon: <SoccerIcon />, label: "축구" },
-    { icon: <FencingIcon />, label: "검도" },
-    { icon: <VolleyballIcon />, label: "배구" },
-    { icon: <BowlingIcon />, label: "볼링" },
-    { icon: <EllipicalIcon />, label: "스피닝" },
-    { icon: <TennisIcon />, label: "스쿼시" },
-    { icon: <WarriorIcon />, label: "자세교정" },
-    { icon: <TennisIcon />, label: "테니스" },
-    { icon: <PiaonIcon />, label: "피아노" },
-    { icon: <JumpRoopIcon />, label: "음악줄넘기" },
-    { icon: <MoreCircleIcon />, label: "기타" },
-  ];
 
   return (
     <div>
@@ -196,47 +64,16 @@ const ProgramFinder = () => {
       <div className="mt-10">
         <div className="flex items-center gap-3 mb-1">
           <p className="font-medium text-body6 text-primary-100">지역</p>
-<<<<<<< HEAD
-<<<<<<< HEAD
           <p className="text-orange-400 text-caption4 font-regular">시 / 도까지 필수입력</p>
-=======
-          <p className="text-orange-400 text-caption4 font-regular">
-            시 / 도까지 필수입력
-          </p>
->>>>>>> 31954a58 (#46 [fix] main merge)
-=======
-          <p className="text-orange-400 text-caption4 font-regular">시 / 도까지 필수입력</p>
->>>>>>> 83414352 (#46 [feat] 버튼 비활성화 설정)
         </div>
       </div>
 
       <RegionDropdown options={regions} onSelect={handleRegionSelect} placeholder="시/도" />
 
       <div className="flex flex-row gap-2 mt-3">
-<<<<<<< HEAD
-        {isRegionSelected && (
-          <RegionDropdown options={states} onSelect={handleStateSelect} placeholder="시/군/구" />
-        )}
-
-        {isSigunguSelected && (
-          <RegionDropdown options={dongs} onSelect={handleDongSelect} placeholder="동/읍/면/리" />
-        )}
-      </div>
-
-      <div className="flex flex-row">
-        {isSelected && 
-          <RegionDropdown options={states} onSelect={handleStateSelect}/>
-        }
-        
-        {isSigunguSelected && 
-          <RegionDropdown options={dongs} onSelect={handleDongSelect}/>
-        }
-      </div>
-=======
         {isSelected && (
           <RegionDropdown options={states} onSelect={handleStateSelect} placeholder="시/군/구" />
         )}
->>>>>>> 83414352 (#46 [feat] 버튼 비활성화 설정)
 
         {isSigunguSelected && (
           <RegionDropdown options={dongs} onSelect={handleDongSelect} placeholder="동/읍/면/리" />
@@ -246,33 +83,12 @@ const ProgramFinder = () => {
       <div className="mt-10">
         <div className="flex items-center gap-3 mb-1">
           <p className="font-medium text-body6 text-primary-100">분야</p>
-<<<<<<< HEAD
-<<<<<<< HEAD
           <p className="text-orange-400 text-caption4 font-regular">여러 개 선택할 수 있어요</p>
         </div>
-<<<<<<< HEAD
-        <div className="flex flex-wrap gap-2">
-=======
-        <div className="flex gap-2 flex-wrap">
->>>>>>> 7d1c7d8e (#52 [Refactor] 버튼 추가)
-=======
-          <p className="text-orange-400 text-caption4 font-regular">
-            여러 개 선택할 수 있어요
-          </p>
-=======
-          <p className="text-orange-400 text-caption4 font-regular">여러 개 선택할 수 있어요</p>
->>>>>>> 83414352 (#46 [feat] 버튼 비활성화 설정)
-        </div>
-        <div className="flex flex-wrap gap-2">
->>>>>>> 31954a58 (#46 [fix] main merge)
-          {fields.map((field, index) => (
-            <FieldButton key={index}>
-              {field.icon}
-              {field.label}
-            </FieldButton>
-          ))}
-        </div>
-
+        <FieldButton>
+          <SwimmingIcon />
+          수영
+        </FieldButton>
       </div>
 
       <div className="mt-[42px]">
