49381f5e41f33c54d539a8d60e89907210202c08
Sohyunnnn <thgus05061@naver.com>
2024-12-07 12:07:03 +0900

    #52 [Style] 스타일 수정

--- src/pages/SignUp/Step2/index.tsx
+++ src/pages/SignUp/Step2/index.tsx
@@ -5,11 +5,13 @@ const Step2 = () => {
   return (
     <>
       <div className="flex flex-col gap-1 mt-10">
-        <label>닉네임</label>
+        <label className="text-primary-100 text-body6 font-medium">닉네임</label>
         <Input />
       </div>
-      <p className="mt-[6px]">8자 이내의 한글/영문/숫자로 입력해주세요.</p>
-      <Button className="mt-[366px] flex bg-orange-400 text-white">다음</Button>
+      <p className="mt-[6px] text-primary-40 text-caption4">8자 이내의 한글/영문/숫자로 입력해주세요.</p>
+      <Button className="mt-[366px] flex bg-orange-400 text-white w-full h-14">
+        다음
+      </Button>
     </>
   );
 };
