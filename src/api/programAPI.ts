e09e0af2b03b89faa7f52e0e497f32c423657f70
yena0213 <yeana3131@naver.com>
2024-12-08 02:02:27 +0900

    #46 [fix] main merge-4

--- src/api/programAPI.ts
+++ src/api/programAPI.ts
@@ -1,1 +1,21 @@
+<<<<<<< HEAD
 //programAPI.ts
+=======
+import { apiGet, apiPost } from "./apiUtil";
+import { DongResponse, ProgramRequest, ProgramResponse, SigunguResponse } from "@api/types/program";
+
+// 2.1 시/군/구 조회
+export const getSigungu = (province: string) => {
+  return apiGet<SigunguResponse>(`/program/sigungu`, {province});
+}
+
+// 2.2 동/읍/면/리 조회
+export const getDong = (province: string, sigungu: string) => {
+  return apiGet<DongResponse>(`/program/dong`, {province, sigungu});
+}
+
+// 2.3 프로그램 조회
+export const postProgram = (data: ProgramRequest, page: number, size: number) => {
+  return apiPost<ProgramResponse>(`/program?page=${page}&size=${size}`, data);
+}
+>>>>>>> 53bc954b (#46 [fix] merge 오류 - 회원가입)
