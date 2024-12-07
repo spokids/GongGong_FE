cf1f20ac40379c0115790b38aa4e1de717bd29cc
yena0213 <yeana3131@naver.com>
2024-12-08 03:56:43 +0900

    #55 [feat] api 등록

--- src/api/programAPI.ts
+++ src/api/programAPI.ts
@@ -1,5 +1,5 @@
-import { apiGet, apiPost } from "./apiUtil";
-import { DongResponse, ProgramRequest, ProgramResponse, SigunguResponse } from "./types/program";
+import { apiGet, apiPost, authApiDelete, authApiGet, authApiPost } from "./apiUtil";
+import { DongResponse, postProgramReviewResponse, ProgramDetailResponse, ProgramRequest, ProgramResponse, ProgramReviewListResponse, SigunguResponse } from "./types/program";
 
 // 2.1 시/군/구 조회
 export const getSigungu = (province: string) => {
@@ -14,4 +14,56 @@ export const getDong = (province: string, sigungu: string) => {
 // 2.3 프로그램 조회
 export const postProgram = (data: ProgramRequest, page: number, size: number) => {
   return apiPost<ProgramResponse>(`/program?page=${page}&size=${size}`, data);
+}
+
+// 2.4 프로그램 상세 조회
+export const getProgramDetail = (programId: number) => {
+  return authApiGet<ProgramDetailResponse>(`/program/${programId}`);
+}
+
+// 2.5 프로그램 스크랩하기
+export const postScrap = (programId: number) => {
+  return authApiPost<void>(`/program/scrap/${programId}`);
+}
+
+// 2.6 프로그램 스크랩 취소
+export const deleteScrap = (programId: number) => {
+  return authApiDelete<void>(`/program/scrap/${programId}`);
+}
+
+// 2.6 프로그램 후기 리스트 조회
+export const getProgramReviewList = (programId: number, lastReviewId?: number, size?: number) => {
+  return apiGet<ProgramReviewListResponse>(`/review/list/${programId}`, {lastReviewId, size});
+}
+
+// 2.7 프로그램 후기 추가
+export const PostProgramReview = (programId: number, content: string, image?: File) => {
+  const formData = new FormData();
+  formData.append("content", content);
+  if (image) {
+    formData.append("image", image);
+  }
+
+  return authApiPost<postProgramReviewResponse>(`/review/${programId}`, formData, undefined);
+  // 자동으로 headers에 multipart/form-data 설정되지 않을 경우
+  // return tokenInstance.post<postProgramReviewResponse>(`/review/${programId}`, formData, {
+  //   headers: {
+  //     "Content-Type": "multipart/form-data",
+  //   },
+  // });
+};
+
+// 2.8 인기 종목 Top 3 조회
+export const getProgramTop3 = () => {
+  return apiGet<string[]>(`/program/top3`);
+}
+
+// 2.9 최신 후기 달린 프로그램 리스트 조회
+export const getProgramReviewed = (page?: number, size?: number) => {
+  return apiGet<ProgramResponse>(`/program/reviewed`, {page, size});
+}
+
+// 2.10 후기 신고하기
+export const postReviewReport = (reviewId: number, reports: string[]) => {
+  return authApiPost<void>(`/review/report/${reviewId}`, {reports});
 }
--- src/api/types/program.ts
+++ src/api/types/program.ts
@@ -1,7 +1,6 @@
 // 2.1
-
 export interface SigunguResponse {
-    sigungu: string[];
+  sigungu: string[];
 }
 
 // 2.2
@@ -18,7 +17,7 @@ export interface ProgramRequest {
   types?: string[];
 }
 
-// 2.3
+// 2.3, 2.9
 export interface ProgramResponse {
   programList: {
     programId: number;
@@ -37,3 +36,40 @@ export interface ProgramResponse {
   totalPage: number;
   currentPage: number;
 }
+
+// 2.4
+export interface ProgramDetailResponse {
+  programId: number;
+  programName: string;
+  programType: string;
+  leaderQualification: boolean;
+  facultyName: string;
+  address: string;
+  pargarmAge: string;
+  programRecruitNumber: number;
+  programDate: string;
+  programWeekDay: string[];
+  programTime: string;
+  homepageUrl: string;
+  isScripted: boolean;
+  reviewCount: number;
+}
+
+// 2.6 프로그램 후기 리스트 조회
+export interface ProgramReviewListResponse {
+  reviews: {
+    reviewId: number;
+    nickName: string;
+    content: string;
+    imageUrl: string;
+    createdAt: string;
+  }[];
+  haxNext: boolean;
+}
+
+// 2.7
+export interface postProgramReviewResponse {
+  reviewId: number;
+}
+
+// 2.8 - string[] 바로 사용
