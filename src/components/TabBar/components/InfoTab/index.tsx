7de077fb1763c8a854c08136420296ba9e08619e
yena0213 <yeana3131@naver.com>
2024-12-09 01:22:20 +0900

    #61 [feat] tabbar design

---
+++ src/components/TabBar/components/InfoTab/index.tsx
@@ -0,0 +1,33 @@
+import { useState } from "react";
+import { ScrabIcon } from "@assets/svg";
+import Button from "@components/Button";
+
+const InfoTab = () => {
+  const [isScrabbed, setIsScrabbed] = useState(false);
+
+  const handleScrabClick = () => {
+    setIsScrabbed((prev) => !prev);
+  };
+
+  return (
+    <div className="fixed bottom-0 left-0 right-0 flex justify-center">
+      <div className="flex justify-between max-w-[410px] w-full py-5 gap-3 bg-white">
+        <Button className={`flex justify-center p-5 h-11 ${
+            isScrabbed ? "bg-orange-50" : "bg-primary-5"
+          }`}  onClick={handleScrabClick}
+        >
+          <ScrabIcon className={isScrabbed ? "text-orange-400" : "text-primary-60"} />
+        </Button>
+        <Button style="filled" className="flex justify-center w-full p-5 text-white h-11">
+          <div className="flex flex-row gap-2">
+            <p>100,000원</p>
+            <p>|</p>
+            <p>수강 신청하러가기</p>
+          </div>
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default InfoTab;
--- src/components/TabBar/index.tsx
+++ src/components/TabBar/index.tsx
@@ -1,13 +1,17 @@
-import HomeTab from "./components/HomeTab";
-import MessageTab from "./components/MessageTab";
+import { HomeIcon, MessageIcon } from "@assets/svg";
+import Button from "@components/Button";
 
 const TabBar = () => {
   return (
-    <div className="fixed bottom-0 left-0 right-0 flex justify-center">
-      <div className="flex justify-between max-w-[410px] w-full py-[9px] gap-3 bg-white">
-        <HomeTab />
-        <MessageTab />
-      </div>
+    <div className="fixed bottom-0 left-0 right-0 flex justify-center py-[9px]  gap-3 bg-white">
+      <Button className="flex gap-2 py-2 w-[170px] px-11">
+        <HomeIcon />
+        <p className="font-normal text-caption2">홈</p>
+      </Button>
+      <Button style="filled" className="px-6 py-2 rounded-lg w-[160px] h-11">
+        <MessageIcon />
+        <p className="font-normal text-caption2">스포키톡</p>
+      </Button>
     </div>
   );
 };
