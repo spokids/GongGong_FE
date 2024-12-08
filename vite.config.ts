b7d545b2d2a2aa2a8dba3dd8b9236b17e5aac31e
Sohyunnnn <thgus05061@naver.com>
2024-12-08 23:57:08 +0900

    Chore: 버셀 배포 오류 해결

--- vite.config.ts
+++ vite.config.ts
@@ -3,31 +3,13 @@ import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 import svgr from "vite-plugin-svgr";
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tsconfigPaths(),
     svgr({
-      include: '**/*.svg?react',
+      include: "**/*.svg?react",
     }),
   ],
-  build: {
-    rollupOptions: {
-      input: '/src/main.tsx',
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            const modulePath = id.split('node_modules/')[1];
-            const topLevelFolder = modulePath.split('/')[0];
-            if (topLevelFolder !== '.pnpm') {
-              return topLevelFolder;
-            }
-            const scopedPackageName = modulePath.split('/')[1];
-            const chunkName = scopedPackageName.split('@')[scopedPackageName.startsWith('@') ? 1 : 0];
-            return chunkName;
-          }
-        },
-      },
-    },
-  },
 });
