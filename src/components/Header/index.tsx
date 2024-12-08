f6d03292b927127e40d918d1e077e3796d4392d9
Sohyunnnn <thgus05061@naver.com>
2024-12-08 23:11:55 +0900

    Feat: 헤더 마이페이지로 이동 및 토큰 유효성 검사

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,26 @@
-import { useLocation, useParams } from "react-router-dom";
+import { useLocation, useNavigate, useParams } from "react-router-dom";
 import { UserIcon } from "@assets/svg";
 import { useMemo } from "react";
+import routes from "constants/routes";
 
 const Header = () => {
   const location = useLocation();
   const { programId } = useParams<{ programId: string }>();
+  const navigate = useNavigate();
+
+  const isTokenValid = (token: string | null) => {
+    if (!token) return false;
+
+    try {
+      const payload = JSON.parse(atob(token.split('.')[1]));
+      const expiration = payload.exp;
+      const currentTime = Math.floor(Date.now() / 1000);
+      return expiration > currentTime;
+    } catch (error) {
+      console.error("JWT 디코딩 오류:", error);
+      return false;
+    }
+  };
 
   const { title, needUserIcon } = useMemo(() => {
     let title = "홈";
@@ -27,10 +43,19 @@ const Header = () => {
     return { title, needUserIcon: iconNeeded };
   }, [location.pathname, programId]);
 
+  const handleUserIconClick = () => {
+    const token = localStorage.getItem("accessToken");
+    if (isTokenValid(token)) {
+      navigate(routes.MyPage);
+    } else {
+      navigate(routes.signIn);
+    }
+  };
+
   return (
     <header className="flex items-center justify-between px-4 py-[10px]">
-      <h1 className={`text-title1 font-bold ${needUserIcon ? 'text-center': 'flex-1 text-left'}`}>{title}</h1>
-      {needUserIcon && <UserIcon />}
+      <h1 className={`text-title1 font-bold ${needUserIcon ? 'text-center' : 'flex-1 text-left'}`}>{title}</h1>
+      {needUserIcon && <UserIcon type="button" className="cursor-pointer" onClick={handleUserIconClick} />}
     </header>
   );
 };
