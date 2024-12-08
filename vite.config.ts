1a435d563cbedbeabaf56bd884107a9db788b879
yena0213 <yeana3131@naver.com>
2024-12-08 20:42:30 +0900

    불필요한 코드 삭제

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import tsconfigPaths from "vite-tsconfig-paths";
 import svgr from "vite-plugin-svgr";
 
 export default defineConfig({
-  base: '/', 
   plugins: [
     react(),
     tsconfigPaths(),
