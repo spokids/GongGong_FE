1e03439f23ada0dbcb49896682df2dc20ef15559
yena0213 <yeana3131@naver.com>
2024-12-08 19:10:26 +0900

    vite 환경변수 변경

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tsconfigPaths from "vite-tsconfig-paths";
 import svgr from "vite-plugin-svgr";
 
 export default defineConfig({
-  base: '/your-app/', 
+  base: '/', 
   plugins: [
     react(),
     tsconfigPaths(),
