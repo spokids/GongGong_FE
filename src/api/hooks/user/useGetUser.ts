65f73924af2b8431e0dfa2578bab46b09890190f
Sohyunnnn <thgus05061@naver.com>
2024-12-08 17:13:06 +0900

    #54 [Style] 스타일 수정

--- src/api/hooks/user/useGetUser.ts
+++ src/api/hooks/user/useGetUser.ts
@@ -3,7 +3,7 @@ import { useQuery } from "@tanstack/react-query";
 
 export const useGetUser = () => {
   return useQuery({
-    queryKey: ["clients"],
+    queryKey: ["user"],
     queryFn: getUser,
     select: (data) => data.data,
   });
